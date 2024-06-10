b002642933757a4185a7acdb9d9e32163ad2596c
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-06-11 01:19:53 +0600

    solved the medain problem

---
+++ Answers/median.py
@@ -0,0 +1,18 @@
+class Solution:
+    def medain(self, n, bit_array):
+        zero_count = 0
+        for bit in bit_array:
+            if bit == 0:
+                zero_count += 1
+        
+        if zero_count < (n/2):
+            print(1)
+        else:
+            print(0)
+
+
+
+if __name__=="__main__":
+    n = int(input("Enter n: "))
+    bit_array = list(map(int, input("Type the bit array: ").split()))
+    Solution().medain(n, bit_array)
